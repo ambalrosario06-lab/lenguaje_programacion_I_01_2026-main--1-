1fa378f867bd1cf9bf47aae28123515be128d66e
ambalrosario06-lab <ambalrosario06@gmail.com>
2026-04-08 09:59:07 -0400

    Update app_window.py

--- lenguaje_programacion_I_01_2026-main/clase_siete/ui/app_window.py
+++ lenguaje_programacion_I_01_2026-main/clase_siete/ui/app_window.py
@@ -1,20 +1,64 @@
 import tkinter as tk
-# from tkinter import ttk
-from service.task_service import TaskService
+from tkinter import messagebox
 
-class AppWindow(tk.Tk):
+class AppWindow:
+    def __init__(self, service):
+        self.service = service
 
-    def __init__(self, task_service: TaskService) -> None:
-        super().__init__()
-        self._task_service = task_service
+        
+        self.root = tk.Tk()
+        self.root.title("Gestor de Tareas")
+        self.root.geometry("400x400")
 
-        self.title("Tkinter desde POO")
-        self.geometry("500x500")
-        self.resizable(False, False)
+        
+        self.label_title = tk.Label(self.root, text="Título:")
+        self.label_title.pack()
 
-        # Widgets
-        self.create_widgets()
+        self.entry_title = tk.Entry(self.root, width=40)
+        self.entry_title.pack()
 
-    def create_widgets(self) -> None:
-        label = tk.Label(self, text="Bienvenido a mi App")
-        label.pack()
+        self.label_desc = tk.Label(self.root, text="Descripción:")
+        self.label_desc.pack()
+
+        self.entry_desc = tk.Entry(self.root, width=40)
+        self.entry_desc.pack()
+
+        self.btn_add = tk.Button(self.root, text="Agregar Tarea", command=self.add_task)
+        self.btn_add.pack(pady=10)
+
+       
+        self.listbox = tk.Listbox(self.root, width=50)
+        self.listbox.pack(pady=10)
+
+      
+        self.load_tasks()
+
+    def add_task(self):
+        title = self.entry_title.get()
+        description = self.entry_desc.get()
+
+        if not title or not description:
+            messagebox.showwarning("Error", "Debe completar todos los campos")
+            return
+
+        # Crear tarea
+        self.service.create_task(title, description)
+
+        # Limpiar inputs
+        self.entry_title.delete(0, tk.END)
+        self.entry_desc.delete(0, tk.END)
+
+        # Recargar lista
+        self.load_tasks()
+
+    def load_tasks(self):
+        self.listbox.delete(0, tk.END)
+
+        tasks = self.service.get_tasks()
+        for task in tasks:
+            text = f"{task['id']}: {task['title']} - {task['description']}"
+            self.listbox.insert(tk.END, text)
+
+    def run(self):
+        self.root.mainloop()
+
